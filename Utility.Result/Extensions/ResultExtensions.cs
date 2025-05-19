975e5de15bf3c181e0baafc8be37ba971a01abf3
Nikita Smolianitski <n.smolianitski@gmail.com>
2025-05-19 15:16:52 +0300

    [Result] Add GetRequestError extension

---
+++ Utility.Result/Extensions/ResultExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BaboonCo.Utility.Result.ResultErrors;
+
+namespace BaboonCo.Utility.Result.Extensions;
+
+public static class ResultExtensions
+{
+    public static RequestError GetRequestError<T>(this FluentResults.Result<T> result)
+    {
+        var grpcError = result.Errors.OfType<RequestError>().FirstOrDefault();
+        if (grpcError is null)
+            throw new NullReferenceException("Required gRPC result error is null.");
+
+        return grpcError;
+    }
+}
